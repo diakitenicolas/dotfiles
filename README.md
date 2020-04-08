a748415c8c5f6dedfe8c8c62bc3e13163498c924
Nicolas Diakite <diakitenicolas@gmail.com>
2020-04-08 17:49:15 +0200

    Improve many stuffs

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # dotfiles
 Dotfiles
+
+
+## Install
+
+mkdir -p ${HOME}/dev/
+cd ${HOME}/dev/
+git clone https://github.com/diakitenicolas/dotfiles.git
+./dotfiles/init
+
+
+## Conf
+
+Use the `PROCESS_DATA` to set tools to not install , tools installation version and others stuffs.
+
+To remove tools from installation use `DISABLE_TOOLS_INSTALL` . 
+Exemple : PGP from install process : 
+```
+DISABLE_TOOLS_INSTALL=BCRYPT
+```
+
+You can also disable all tools using the `ALL` value.
+```
+DISABLE_TOOLS_INSTALL=ALL
+```
+
+To set Java version to use : 
+```
+JDK_VERSION_TO_USE=1.8
+```
+
+
+To set GOLANG version to use : 
+```
+GO_VERSION_TO_INSTALL=1.14.1 
+```
--- install.sh
+++ install.sh
@@ -1,83 +1,141 @@
-#!/usr/bin/env bash
 
+#!/usr/bin/env sh
 
-readonly CURRENT_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CURRENT_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-printTitle(){
-	local line='-----------------------------------------------------'
-	printf "%s%s%s\n" "+-" "${line:0:${#1} }" "-+"
-	printf "%s%s%s\n" "| " "${1}" " |"
-	printf "%s%s%s\n" "+-" "${line:0:${#1}}" "-+"
+print_title(){
+  local line='-----------------------------------------------------'
+  printf "%s%s%s\n" "+-" "${line:0:${#1} }" "-+"
+  printf "%s%s%s\n" "| " "${1}" " |"
+  printf "%s%s%s\n" "+-" "${line:0:${#1}}" "-+"
 }
 
 
-printSubTitle(){
-	printf "\n%s%s\n" "--> " "${1}"
+print_sub_title(){
+  printf "\n%s%s\n" "--> " "${1}"
 }
 
 man() {
     printf "Usage of %s\n" "${0}"
+
+
+    printf "all\n"
+    printf "\t Launch Symlink , Clean and install \n"
+
     printf "symlinks\n"
-    printf "\t Create symlink of dotfiles into \${HOME}\n"
+    printf "\t Create symlink of dotfiles into \${HOME} \n"
     
+    printf "clean\n"
+    printf "\t Clean installation and temporary files\n"
+    
+
     printf "install\n"
     printf "\t Install required softwares\n"
-    
+
+   
     printf "[blank]\n"
     printf "\t Run all stages\n"
+    
     printf "man\n"
     printf "\t Print this man\n"
+
+
+    printf "During processing the file  PROCESS_DATA is used to determinate which tools must be process for action. The values are \n"
+  printf "\t  - DISABLE_TOOLS_INSTALL : Example DISABLE_TOOLS_INSTALL=DOCKER means docker will not process for any action \n"    
 }
 
 
-createSymlinks(){
-	for file in ${CURRENT_DIR}/symlinks/* ; do
-		basenameFile=$(basename "${file}")
-		[ -r "$file" ] && [ -f "$file" ] &&  [ -e "$file" ]  && rm -f  && ln -sf "${file}" "${HOME}/".${basenameFile};	
-	done
-}
+create_symlinks(){
 
-
-installTools() {
-  local installing="Installing"
-  for file in "${CURRENT_DIR}/tools"/*; do
-    local basenameFile=$(basename ${file%.*}) 
-    local upperCaseFilename=$(echo "${basenameFile}" | tr '[:lower:]' '[:upper:]')
-    local disableVariableName="DOTFILES_NO_${upperCaseFilename}"
-
-    if [[ "${!disableVariableName:-}" == "true" ]]; then
-      continue
-    fi
-
-    printSubTitle "${installing} ${basenameFile}"
-    [ -r "$file" ] && [ -f "$file" ] && source "$file";
+  for file in ${CURRENT_DIR}/symlinks/* ; do
+    BASENAME_FILE=$(basename "${file}")
+    echo "${HOME}/.${BASENAME_FILE}"
+    [ -r "$file" ] && [ -f "$file" ] &&  [ -e "$file" ]  && rm -f "${HOME}/.${BASENAME_FILE}" && ln -s "${file}" "${HOME}/".${BASENAME_FILE}; 
   done
 }
 
+process_tools() {
+
+    local LC_ALL="C"
+    local LANG="C"
+    local PROCESS_DATA_FILE_PATH="${CURRENT_DIR}/PROCESS_DATA"
+    local DISABLE_TOOLS_INSTALLATION=$( grep ^DISABLE_TOOLS ${PROCESS_DATA_FILE_PATH} | cut -d '=' -f2) 
+
+
+    if [[ "$DISABLE_TOOLS_INSTALLATION" == "ALL" ]]; then
+      printf "ALL TOOLS ARE DEACTIVATE FOR PROCESSING - SEE PROCESS_DATA file \n"
+      return ;
+  fi
+
+  for file in "${CURRENT_DIR}/tools"/*; do
+    local BASENAME_FILE
+      BASENAME_FILE="$(basename "${file%.*}")"
+
+      local UPPERCASE_FILENAME
+      UPPERCASE_FILENAME="$(echo "${BASENAME_FILE}" | tr "[:lower:]" "[:upper:]")"
+
+
+      if [[ "$DISABLE_TOOLS_INSTALLATION" == *"$UPPERCASE_FILENAME"* ]]; then
+        #echo  $UPPERCASE_FILENAME "is DEACTIVATE FOR PROCESSING - SEE PROCESS_DATA file"
+        continue ;
+    fi
+
+
+      if [[ -r ${file} ]]; then
+          for action in "${@}"; do
+            unset -f "${action}"
+        done
+
+          source "${file}"
+
+          for action in "${@}"; do
+
+            if [[ $(type -t "${action}") = "function" ]]; then
+              print_sub_title "${action} - ${BASENAME_FILE}"
+               "${action}"
+            fi
+          done
+      fi
+    done
+}
+
+clean_brew() {
+  if command -v brew > /dev/null 2>&1; then
+      brew cleanup
+    fi
+}
+
 main(){
-	
+
   local ARGS="${*}"
-
-   if [[ "${ARGS}" =~ man ]]; then
-    usage
-    return 1
+    
+  if [[ "${ARGS}" =~ man ]]; then
+      man
+      return 0
   fi
 
-  if [[ -z "${ARGS}" ]] || [[ "${ARGS}" =~ symlinks ]]; then
-    printTitle "Creation Symlinks"
-    createSymlinks
+  if [[ "${ARGS}" == "all" ]] || [[ "${ARGS}" =~ symlinks ]]; then
+  print_title "Creation Symlinks"
+  create_symlinks
   fi
 
-	set +u
+  set +u
   set +e
-	printTitle "Launch bashrc"
-	PS1="$" source "${HOME}/.bashrc"
-	set -e
-	set -u
+  print_title "Launch bashrc"
+  PS1="$" source "${HOME}/.bashrc"
+  set -e
+  set -u
 
-  if [[ -z "${ARGS}" ]] || [[ "${ARGS}" =~ install ]]; then
-    printTitle "Installation"
-    installTools
+  if [[ "${ARGS}" =~ all ]] || [[ "${ARGS}" =~ clean ]]; then
+    print_title "Cleaning"
+    process_tools clean
+    clean_brew
+  fi
+  
+  if [[ "${ARGS}" =~ all ]] || [[ "${ARGS}" =~ install ]]; then
+    print_title "Installation"
+    process_tools install
+  clean_brew
   fi
 
 }
